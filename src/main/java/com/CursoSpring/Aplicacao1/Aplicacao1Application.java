3252607126105663a014c00b27bb4cfb89dc24bf
Alexandre O Marinho <alexandreomar0202@gmail.com>
2023-01-02 20:49:38 -0300

    Corrigindo Erro de DDL

--- src/main/java/com/CursoSpring/Aplicacao1/Aplicacao1Application.java
+++ src/main/java/com/CursoSpring/Aplicacao1/Aplicacao1Application.java
@@ -2,12 +2,14 @@ package com.CursoSpring.Aplicacao1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Aplicacao1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aplicacao1Application.class, args);
+		System.out.print(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
--- src/main/java/com/CursoSpring/Aplicacao1/model/Usuario.java
+++ src/main/java/com/CursoSpring/Aplicacao1/model/Usuario.java
@@ -4,12 +4,16 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Collection;
 
 @Entity
 public class Usuario implements UserDetails {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
     private String login;
     private String nomeCompleto;
     private String senha;
@@ -37,6 +41,14 @@ public class Usuario implements UserDetails {
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
+   /* public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }*/
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
